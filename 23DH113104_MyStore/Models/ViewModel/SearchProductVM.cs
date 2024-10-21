a5b8bab4cf048e36a057be79641ecc03c921c552
tangdeptrai <tangpham64@gmail.com>
2024-10-21 21:45:34 +0700

    Create SearchProductVM.cs

---
+++ 23DH113104_MyStore/Models/ViewModel/SearchProductVM.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _23DH113104_MyStore.Models.ViewModel
+{
+    public class SearchProductVM
+    {
+    }
+}
