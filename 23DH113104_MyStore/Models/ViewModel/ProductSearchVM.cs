9e9536b6b3e5367002875c619c4713fce20a56be
tangdeptrai <tangpham64@gmail.com>
2024-10-21 21:45:30 +0700

    Create ProductSearchVM.cs

---
+++ 23DH113104_MyStore/Models/ViewModel/ProductSearchVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _23DH113104_MyStore.Models.ViewModel
+{
+    public class ProductSearchVM
+    {
+        //tiêu chí để search theo tên, mô tả sp
+        //hoặc loại sản phẩm
+        public string SearchTerm { get; set; }
+        public List<Product> Products { get; set; }
+    }
+}
